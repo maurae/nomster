265800cf42683170c936cf7ea96ee1c555de8d61
maurae <mauraeba@gmail.com>
2016-03-05 23:59:05 +0000

    added items to see pagination

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
 	def index
-		@places = Place.all.paginate(page: params[:page], per_page: 2)
+		@places = Place.all.paginate(page: params[:page], per_page: 1)
 	end
 end
