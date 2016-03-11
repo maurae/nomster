50285e77f6c51aa242423c0f4c307afdc60e1ac4
maurae <mauraeba@gmail.com>
2016-03-11 05:13:24 +0000

    Secure delete/edit options

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 	def index
 		@places = Place.all.paginate(page: params[:page], per_page: 2)
 	end
@@ -19,23 +19,17 @@ class PlacesController < ApplicationController
 
 	def edit
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render text: 'Not Allowed', status: :forbidden
+		end
 	end
 
 	def update
 		@place = Place.find(params[:id])
-		@place.update_attributes(place_params)
-		redirect_to root_path
+		if @place.user != current_user
+			return render text: 'Not Allowed', status: :forbidden
 	end
 
-	def destroy
-	  @place = Place.find(params[:id])
-	  @place.destroy
-	  redirect_to root_path
-	end
-
-	private
-
-	def place_params
-		params.require(:place).permit(:name, :description, :address)
-	end
+	@place.update_attributes(place_params)
+	redifrect_to root_path
 end
