61e1d380ecce08ce76dbb10ad992e823bf1a8e06
vagrant <vagrant@precise32.(none)>
2016-03-28 02:31:25 +0000

    undefined local variable place

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,9 +2,9 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added(comment)
-  	@place =comment.place
-  	@place_owner =place.user
+  	@place = comment.place
+  	@place_owner = place.user
   	mail(to: @place_owner.email, 
-  		subject: "A comment has been added to your place")
+  		subject: "A comment has been added to #{@place.name}")
   end
 end
