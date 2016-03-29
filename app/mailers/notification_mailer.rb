211e4ed022d06dc0ec7a2b3abb5e2a3a1ab44315
vagrant <vagrant@precise32.(none)>
2016-03-29 01:38:04 +0000

    changing comment to Comment

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added(comment)
-  	@place = comment.place
+  	@place = Comment.place
   	@place_owner = @place.user
   	mail(to: @place_owner.email, 
   		subject: "A comment has been added to #{@place.name}")
