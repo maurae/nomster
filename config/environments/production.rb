a77bf060526f63b88881b3016865a5d45aef1499
vagrant <vagrant@precise32.(none)>
2016-03-30 21:30:03 +0000

    everything working except for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Nomster::Application.configure do
-  config.action_mailer.default_url_options = { host: 'https://nomster-maurae.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'https://nomster-maurae.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
